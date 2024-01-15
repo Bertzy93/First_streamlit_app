58343486ad2c098a9ca58f9abd8640ac73e0504a
Bertzy93 <156704404+Bertzy93@users.noreply.github.com>
2024-01-16 02:56:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ streamlit.title ('My Parents New healthy Diner')
 
 streamlit.header ('Breakfast Menu')
 
-streamlit.text ('Omega 3 and Blueberry Oatmeal')
+<B>streamlit.text ('Omega 3 and Blueberry Oatmeal')
 streamlit.text ('Kale, Spinach & Rocket Smoothie')
 streamlit.text ('Hard-Boiled Free-Range Egg')
 streamlit.text ('hard-Boiled Free-Range Egg')
