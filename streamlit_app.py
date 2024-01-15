aeb574a0b3949c07d3d98e1daf57c94d87d58095
Bertzy93 <156704404+Bertzy93@users.noreply.github.com>
2024-01-16 02:52:08 +0800

    Update streamlit_app.py
    additional some contents

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit
 streamlit.title ('My Parents New healthy Diner')
 
 streamlit.header ('Breakfast Menu')
+
+streamlit.content ('Omega 3 and Blueberry Oatmeal')
+streamlit.content ('Kale, Spinach & Rocket Smoothie')
+streamlit.content ('Hard-Boiled Free-Range Egg')
