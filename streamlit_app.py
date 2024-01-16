a3e3fe110c2cf12b0a2d4346598c7d35fc391a18
Bertzy93 <156704404+Bertzy93@users.noreply.github.com>
2024-01-17 04:27:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,6 +49,10 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+streamlit.text("What would like to add?!")
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','apple')
+streamlit.write('The user entered ', add_my_fruit)
+streamlit.text("Thanks for adding " + add_my_fruit)
 
 # import snowflakes
 
