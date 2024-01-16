3ccfefacdded2bd454368e513548c5b990322b30
Bertzy93 <156704404+Bertzy93@users.noreply.github.com>
2024-01-17 01:03:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.dataframe(fruits_to_show)
 
 import requests
 String1 = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text('String1')
+streamlit.text(String1)
 
 
 
