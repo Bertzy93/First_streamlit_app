2e8b1ab6df8fed2d8c2a29e151a1e2e127d245fc
Bertzy93 <156704404+Bertzy93@users.noreply.github.com>
2024-01-17 05:06:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,22 +30,17 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+      fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+      streamlit.error("Please select a fruit to get information")
+  else:
+      String1 = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      String2 = pandas.json_normalize(String1.json())
+      streamlit.dataframe(String2)
+except URLError as e:
+    streamlit.error()
 
-
-String1 = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# streamlit.text(String1.json())
-# Pass String1 to String2
-String2 = pandas.json_normalize(String1.json())
-# write your own comment - what does this do?
-streamlit.dataframe(String2)
-
-#don't run anything past here while we troubleshoot
-
-
-#requirement.txt
-# snowflake-connector-phyton
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -65,64 +60,3 @@ streamlit.subheader ('By: Chef Bert Lagro')
 streamlit.video("https://youtu.be/EdSLy-CyHes", format='video/mp4', start_time=0)
 streamlit.stop()
 
-
-
-# import streamlit as st
-# import streamlit
-# import pandas
-# import requests
-# # import snowflake.connector
- 
-# streamlit.title('My Parents New Healthy Diner')
- 
-# streamlit.header('Breakfast Menu')
-# streamlit.text('Omega 3 & Blueberry Oatmeal')
-# streamlit.text('Kale, Spinach & Rocket Smoothie')
-# streamlit.text('Hard-Boiled Free-Range Egg')
- 
-# streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-# my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
- 
-# # Display the table on the page.
-# my_fruit_list = my_fruit_list.set_index('Fruit')
- 
-# # Let's put a pick list here so they can pick the fruit they want to include 
-# fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),[])
-# fruits_to_show = my_fruit_list.loc[fruits_selected]
- 
-# streamlit.dataframe(fruits_to_show)
- 
-# streamlit.header("Fruityvice Fruit Advice!")
- 
-# fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-# streamlit.write('The user entered ', fruit_choice)
- 
-# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-# # streamlit.text(fruityvice_response.json())
- 
- 
-# # write your own comment -what does the next line do? 
-# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# # write your own comment - what does this do?
-# streamlit.dataframe(fruityvice_normalized)
- 
-# #streamlit.stop()
- 
-# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-# my_cur = my_cnx.cursor()
-# # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
- 
-# my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-# my_data_rows = my_cur.fetchall()
-# #streamlit.text("Hello from Snowflake:")
-# streamlit.header("The fruit list contains")
-# streamlit.dataframe(my_data_rows)
- 
- 
-# streamlit.text("What fruit would you like to add?")
-# text_input = st.text_input("Enter some text 👇")
- 
-# if text_input:
-#     st.write("Thanks for adding : ", text_input)
- 
-# my_cur.execute("insert into fruit_load_list values ('from streamlit')")
