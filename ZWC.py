b00ff2988d920112b4b64820bd96ca8bc6bec7f7
Bertzy93 <156704404+Bertzy93@users.noreply.github.com>
2024-01-23 23:15:17 +0800

    Update ZWC.py

--- ZWC.py
+++ ZWC.py
@@ -34,9 +34,9 @@ streamlit.title('Zena\'s Amazing Athleisure Catalog')
 # streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/yellow_sweatsuit.png")
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),
-CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+# my_cur = my_cnx.cursor()
+# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),
+# CURRENT_REGION()")
+# my_data_row = my_cur.fetchone()
+# streamlit.text("Hello from Snowflake:")
+# streamlit.text(my_data_row)
