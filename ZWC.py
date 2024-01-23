73a8b9feb7ae3fcdfea79067581113a4258526a6
Bertzy93 <156704404+Bertzy93@users.noreply.github.com>
2024-01-24 01:04:05 +0800

    Update ZWC.py

--- ZWC.py
+++ ZWC.py
@@ -3,40 +3,35 @@ import snowflake.connector
 import pandas
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
 
-# streamlit.header("90s tracksuit")
-# streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/90s_tracksuit.png")
 
-# streamlit.header("burgundy_sweatsuit")
-# streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/burgundy_sweatsuit.png")
+streamlit.header("90s tracksuit")
+streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/90s_tracksuit.png")
 
-# streamlit.header("charcoal_grey_sweatsuit")
-# streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/charcoal_grey_sweatsuit.png")
+streamlit.header("burgundy sweatsuit")
+streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/burgundy_sweatsuit.png")
 
-# streamlit.header("forest_green_sweatsuit")
-# streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/forest_green_sweatsuit.png")
+streamlit.header("charcoal grey sweatsuit")
+streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/charcoal_grey_sweatsuit.png")
 
-# streamlit.header("navy_blue_sweatsuit")
-# streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/navy_blue_sweatsuit.png")
+streamlit.header("forest green sweatsuit")
+streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/forest_green_sweatsuit.png")
 
-# streamlit.header("orange_sweatsuit")
-# streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/orange_sweatsuit.png")
+streamlit.header("navy blue sweatsuit")
+streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/navy_blue_sweatsuit.png")
 
-# streamlit.header("purple_sweatsuit")
-# streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/purple_sweatsuit.png")
+streamlit.header("orange sweatsuit")
+streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/orange_sweatsuit.png")
 
-# streamlit.header("red_sweatsuit")
-# streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/red_sweatsuit.png")
+streamlit.header("purple sweatsuit")
+streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/purple_sweatsuit.png")
 
-# streamlit.header("royal_blue_sweatsuit")
-# streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/royal_blue_sweatsuit.png")
+streamlit.header("red sweatsuit")
+streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/red_sweatsuit.png")
 
-# streamlit.header("yellow_sweatsuit")
-# streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/yellow_sweatsuit.png")
+streamlit.header("royal blue sweatsuit")
+streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/royal_blue_sweatsuit.png")
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-# my_cur = my_cnx.cursor()
-# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),
-# CURRENT_REGION()")
-# my_data_row = my_cur.fetchone()
-# streamlit.text("Hello from Snowflake:")
-# streamlit.text(my_data_row)
+streamlit.header("yellow sweatsuit")
+streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/yellow_sweatsuit.png")
+
+
