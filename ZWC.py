24a52f63bf35af3d2d1d4c641fe9d4a29b20b64f
Bertzy93 <156704404+Bertzy93@users.noreply.github.com>
2024-01-24 01:38:45 +0800

    Update ZWC.py

--- ZWC.py
+++ ZWC.py
@@ -24,6 +24,8 @@ Sweat_selected = streamlit.multiselect("Select Sweat Clothings:",
 IF Sweat_selected == "90s tracksuit":
 streamlit.header("90s tracksuit")
 streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/90s_tracksuit.png")
+ELSE:
+streamlit.header("No Selection")
 
 
 # streamlit.header("burgundy sweatsuit")
