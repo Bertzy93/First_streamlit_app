88557e40a06e46da9dce49698dfc10c0f973bf62
Bertzy93 <156704404+Bertzy93@users.noreply.github.com>
2024-01-24 01:37:30 +0800

    Update ZWC.py

--- ZWC.py
+++ ZWC.py
@@ -21,35 +21,36 @@ Sweat_selected = streamlit.multiselect("Select Sweat Clothings:",
 )
 
 
-
+IF Sweat_selected == "90s tracksuit":
 streamlit.header("90s tracksuit")
 streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/90s_tracksuit.png")
 
-streamlit.header("burgundy sweatsuit")
-streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/burgundy_sweatsuit.png")
 
-streamlit.header("charcoal grey sweatsuit")
-streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/charcoal_grey_sweatsuit.png")
+# streamlit.header("burgundy sweatsuit")
+# streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/burgundy_sweatsuit.png")
 
-streamlit.header("forest green sweatsuit")
-streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/forest_green_sweatsuit.png")
+# streamlit.header("charcoal grey sweatsuit")
+# streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/charcoal_grey_sweatsuit.png")
 
-streamlit.header("navy blue sweatsuit")
-streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/navy_blue_sweatsuit.png")
+# streamlit.header("forest green sweatsuit")
+# streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/forest_green_sweatsuit.png")
 
-streamlit.header("orange sweatsuit")
-streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/orange_sweatsuit.png")
+# streamlit.header("navy blue sweatsuit")
+# streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/navy_blue_sweatsuit.png")
 
-streamlit.header("purple sweatsuit")
-streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/purple_sweatsuit.png")
+# streamlit.header("orange sweatsuit")
+# streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/orange_sweatsuit.png")
 
-streamlit.header("red sweatsuit")
-streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/red_sweatsuit.png")
+# streamlit.header("purple sweatsuit")
+# streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/purple_sweatsuit.png")
 
-streamlit.header("royal blue sweatsuit")
-streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/royal_blue_sweatsuit.png")
+# streamlit.header("red sweatsuit")
+# streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/red_sweatsuit.png")
 
-streamlit.header("yellow sweatsuit")
-streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/yellow_sweatsuit.png")
+# streamlit.header("royal blue sweatsuit")
+# streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/royal_blue_sweatsuit.png")
+
+# streamlit.header("yellow sweatsuit")
+# streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/yellow_sweatsuit.png")
 
 
