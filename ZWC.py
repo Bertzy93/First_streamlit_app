201541630bfad359137046a9c7b58e45899eb6b2
Bertzy93 <156704404+Bertzy93@users.noreply.github.com>
2024-01-23 21:49:36 +0800

    Update ZWC.py

--- ZWC.py
+++ ZWC.py
@@ -3,4 +3,4 @@ import snowflake.connector
 import pandas
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
 
-st.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/90s_tracksuit.png")
+streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/90s_tracksuit.png")
