caef98ee14e91c28c890d7335655e16f6dcde9d9
Bertzy93 <156704404+Bertzy93@users.noreply.github.com>
2024-01-23 21:48:49 +0800

    Update ZWC.py

--- ZWC.py
+++ ZWC.py
@@ -2,6 +2,5 @@ import streamlit
 import snowflake.connector
 import pandas
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
-import imageio as Imagereader
 
 st.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/90s_tracksuit.png")
