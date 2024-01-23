7dbebb2d0a61de5fb5705ac0865f97f8add980a8
Bertzy93 <156704404+Bertzy93@users.noreply.github.com>
2024-01-23 21:27:58 +0800

    Update ZWC.py

--- ZWC.py
+++ ZWC.py
@@ -1,5 +1,8 @@
 import streamlit
 import pandas
 import requests
+import imageio
 
 streamlit.title ("Zena's Amazing Athleuisure Catalog")
+
+clothes_List = imread("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/90s_tracksuit.png")
