de20064d9a7b2a5d610ef471c210fd866850ef9e
Bertzy93 <156704404+Bertzy93@users.noreply.github.com>
2024-01-23 21:54:48 +0800

    Update ZWC.py

--- ZWC.py
+++ ZWC.py
@@ -4,14 +4,32 @@ import pandas
 streamlit.title('Zena\'s Amazing Athleisure Catalog')
 
 
-streamlit.header("90s_tracksuit")
+streamlit.header("90s tracksuit")
 streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/90s_tracksuit.png")
+
+streamlit.header("burgundy_sweatsuit")
 streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/burgundy_sweatsuit.png")
+
+streamlit.header("charcoal_grey_sweatsuit")
 streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/charcoal_grey_sweatsuit.png")
+
+streamlit.header("forest_green_sweatsuit")
 streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/forest_green_sweatsuit.png")
+
+streamlit.header("navy_blue_sweatsuit")
 streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/navy_blue_sweatsuit.png")
+
+streamlit.header("orange_sweatsuit")
 streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/orange_sweatsuit.png")
+
+streamlit.header("purple_sweatsuit")
 streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/purple_sweatsuit.png")
+
+streamlit.header("red_sweatsuit")
 streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/red_sweatsuit.png")
+
+streamlit.header("royal_blue_sweatsuit")
 streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/royal_blue_sweatsuit.png")
+
+streamlit.header("yellow_sweatsuit")
 streamlit.image("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing/yellow_sweatsuit.png")
